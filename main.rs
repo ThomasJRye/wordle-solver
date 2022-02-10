e906d77a2fd2c7ae41cf1bdd10c612b3d4044a3d
ThomasJRye <thomasjrye@gmail.com>
2022-02-10 11:11:24 +0100

    getwords

--- main.rs
+++ main.rs
@@ -3,31 +3,42 @@ use std::fs;
 
 fn main() {
     let path = "index.txt";
-    let mut contents = fs::read_to_string(path).expect("something wrong");
+    let contents = fs::read_to_string(path).expect("something wrong");
 
-    let mut words = Vec::new();
+    let words = getwords(contents);
 
+    let shortwords = fivelettersonly(words);
+}
+
+fn getwords(wordList: String) -> Vec<String> {
     let mut iterator = 0;
-    let mut lengthRead = 0;
+    let mut length_read = 0;
 
-    println!("With text:\n{}", &contents[0..20]);
+    let mut words = Vec::<String>::new();
 
-    for i in contents.chars() {
+    let length = wordList.len();
+    for i in wordList.chars() {
         if i.is_alphabetic() {
             iterator += 1;
-        } else {
-            if iterator == 5 {
-                words.push(&contents[lengthRead..lengthRead + iterator]);
-
-                println!("{}", &contents[lengthRead..lengthRead + iterator]);
-                println!("lengthRead: {}, iterator: {}", lengthRead, iterator);
-
-                lengthRead += iterator;
-                iterator = 0;
-            } else {
-                lengthRead += iterator;
-                iterator = 0;
-            }
+            length_read += 1;
+        } else if length_read + iterator < length {
+            words.push(wordList[length_read..length_read + iterator].to_string());
+            iterator = 0;
+            length_read += 1;
         }
     }
+
+    return words;
+}
+
+fn fivelettersonly(words: Vec<String>) -> Vec<String> {
+    let mut shortwords = Vec::<String>::new();
+
+    for word in words {
+        if word.len() == 5 {
+            shortwords.push(word.to_string());
+        }
+    }
+
+    return shortwords;
 }
