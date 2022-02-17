329a29adb91610b6dc47d0cc69c39b474aa6135f
thomasjrye <thomasjrye@gmail.com>
2022-02-17 10:08:25 +0100

    added most used letter

--- main.rs
+++ main.rs
@@ -10,8 +10,7 @@ fn main() {
 
     let shortwords = fivelettersonly(words);
 
-    let filtered = charfilter(shortwords, 'd');
-    println!("{:?}", filtered.len());
+    println!("{:?}", words_by_usage(&shortwords));
 }
 
 fn getwords(wordList: String) -> Vec<String> {
@@ -71,4 +70,59 @@ fn contains(word: String, letter: char) -> bool {
     }
 
     return false;
+}
+
+fn letter_counter(words: Vec<String>, letter: char) -> usize {
+    let mut count: usize = 0;
+    for word in words {
+        if word.contains(letter) {
+            count += 1;
+        }
+    }
+
+    return count;
+}
+
+#[derive(Debug, Eq, Ord, PartialEq, PartialOrd)]
+struct Letter {
+    letter: char,
+    uses: usize,
+}
+
+fn words_by_usage(words: &Vec<String>) -> Vec<Letter> {
+    static ASCII_LOWER: [char; 26] = [
+        'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
+        's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+    ];
+
+    let mut letters = Vec::<Letter>::new();
+
+    for letter in ASCII_LOWER {
+        letters.push(Letter {
+            letter: letter,
+            uses: letter_counter(words.to_vec(), letter),
+        })
+    }
+
+    letters.sort_by(|a, b| b.uses.cmp(&a.uses));
+    return letters;
+}
+
+fn get_words_with_most_used_letters(words: &Vec<String>) -> Vec<String> {
+    let mut all_words = words_by_usage(words);
+    let mut letters = Vec::<char>::new();
+    let mut words: Vec<String> = Vec::<String>::new();
+
+    let best_letter_structs = &all_words[0..4];
+
+    for letter in best_letter_structs {
+        letters.push(letter.letter);
+    }
+
+    for word in words {
+        if contains(word, letters) {
+            words.push(word)
+        }
+    }
+    return words;
 }
